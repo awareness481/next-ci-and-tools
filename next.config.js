8b08c0361662e138f235178cf8d4984a6c8e0c88
Jesse <jesse.jafa@gmail.com>
2020-11-07 14:59:15 +0200

    @next/bundle-analyze next-compose-plugins

--- next.config.js
+++ next.config.js
@@ -1,16 +1,25 @@
-const { RelativeCiAgentWebpackPlugin } = require('@relative-ci/agent');
-const withBundleStats = require('next-plugin-bundle-stats');
-
-module.exports = withBundleStats({
-  webpack: function (config, options) {
-    const { dev, isServer } = options;
-
-    if (!dev && !isServer) {
-      config.plugins.push(
-        new RelativeCiAgentWebpackPlugin(),
-      );
-    }
-
-    return config;
-  }
+const { RelativeCiAgentWebpackPlugin } = require("@relative-ci/agent");
+const withBundleStats = require("next-plugin-bundle-stats");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
+const withPlugins = require("next-compose-plugins");
+
+module.exports = withPlugins(
+  [
+    [withBundleAnalyzer({})],
+    withBundleStats(),
+    // your other plugins here
+  ],
+  {
+    webpack: function (config, options) {
+      const { dev, isServer } = options;
+
+      if (!dev && !isServer) {
+        config.plugins.push(new RelativeCiAgentWebpackPlugin());
+      }
+
+      return config;
+    },
+  }
+);
