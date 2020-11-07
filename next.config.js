e2a8147a1601425bf58bb0ed0270276dba8a46e2
Jesse <jesse.jafa@gmail.com>
2020-11-07 14:35:31 +0200

    next bundle-stats

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const { RelativeCiAgentWebpackPlugin } = require('@relative-ci/agent');
+const withBundleStats = require('next-plugin-bundle-stats');
 
-module.exports = {
+module.exports = withBundleStats({
   webpack: function (config, options) {
     const { dev, isServer } = options;
 
@@ -12,4 +13,4 @@ module.exports = {
 
     return config;
   }
-};
+});
